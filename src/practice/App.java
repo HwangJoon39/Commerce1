96c3f384e1b37885bda55a2f8b69b4b4b3905ae6
hwangjoon <hjoon39@naver.com>
2026-03-31 14:35:02 +0900

    Step1 - App , Product 클래스 생성
    전자제품 목록 추가

---
+++ src/practice/App.java
@@ -0,0 +1,40 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Galaxy S25", 1200000, "최신형 안드로이드 스마트폰", 5));
+        products.add(new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 10));
+        products.add(new Product("MacBook Pro", 2400000, "M3칩셋이 탑재된 노트북", 15));
+        products.add(new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 20));
+
+        System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
+        int productNum = 1;
+        for (Product product : products) {
+            System.out.println(productNum + ". " +
+                    product.getName()+ " | " +
+                    product.getPrice()+" | " +
+                    product.getDescription()+" | "+
+                    product.getStock());
+            productNum++;
+        }
+        System.out.println("0. 종료           |프로그램 종료");
+        System.out.print("번호를 입력해주세요: ");
+        sc.nextInt();
+        int num = 0;
+        if (num == 0){
+            System.out.println(" 커머스 플랫폼을 종료하겠습니다. ");}
+
+    }
+}
+
---
+++ src/practice/Product.java
@@ -0,0 +1,45 @@
+package practice;
+
+public class Product {
+
+    //속성
+    private String name;
+    private int price;
+    private String description;
+    private int stock;
+
+
+
+
+    //생성자
+    public Product(String name,int price,String description,int stock ){
+        this.name=name;
+        this.price=price;
+        this.description=description;
+        this.stock=stock;
+
+    }
+
+
+
+
+
+    //기능
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+}
